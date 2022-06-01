3bbfc66cc38f1eab3f51ca7b7718f8b3f1ed2868
Krisanathep Boonpromma <krisanathep.b@gli.co.th>
2022-06-01 13:44:39 +0700

    Wrap content with layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Html from './Html';
+import Layout from './Layout';
 
 export default function App() {
   return (
     <Html title='React App' assets={{}}>
-      <div>Hello react app</div>
+      <Layout>
+        <div>Hello react app</div>
+      </Layout>
     </Html>
   );
 }
